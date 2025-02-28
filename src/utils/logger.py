2d2690bfad327c5c392e1ea002a4c8e1db78402f
Łukasz Łojewski <lukasz.lojewski@softiq.pl>
2025-02-28 14:33:53 +0100

    moved logger config to logger file instead of .ini (wasn't properly displaying error logs for config json files)

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,24 +1,24 @@
 import logging
 
 
-logger = logging.getLogger("Logger")
+logger = logging.getLogger('Logger')
 logger.setLevel(logging.DEBUG)
 
 
 cli_handler = logging.StreamHandler()
 cli_handler.setLevel(logging.DEBUG)
 cli_formatter = logging.Formatter(
-    "%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%H:%M:%S"
+    fmt='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S'
 )
 cli_handler.setFormatter(cli_formatter)
 
 
-file_handler = logging.FileHandler("artifacts/playwright_tests.log", mode="w")
+file_handler = logging.FileHandler(filename='artifacts/playwright_tests.log', mode='w')
 file_handler.setLevel(logging.INFO)
 file_formatter = logging.Formatter(
-    "%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d at %H:%M:%S"
+    fmt='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d at %H:%M:%S'
 )
 file_handler.setFormatter(file_formatter)
 
