4d86f9c0d20ad1f0fb8773eaf67654a5e2833797
Łukasz Łojewski <lukasz.lojewski@softiq.pl>
2025-02-28 14:29:25 +0100

    moved logger config to logger file instead of .ini (wasn't properly displaying error logs for config json files)

--- src/utils/json_data.py
+++ src/utils/json_data.py
@@ -1,9 +1,15 @@
 import json
+from src.utils.logger import logger
 
 
-with (open('./data/database_info.json') as database_info_file,
-      open('./data/user_data.json') as user_data_file,
-      open('./data/sql_queries.json') as sql_queries_file):
-    DATABASE_INFO = json.load(database_info_file)
-    SQL_QUERIES = json.load(sql_queries_file)
-    USER_DATA = json.load(user_data_file)
+try:
+    with (open('./data/database_info.json') as database_info_file,
+          open('./data/user_data.json') as user_data_file,
+          open('./data/sql_queries.json') as sql_queries_file):
+        DATABASE_INFO = json.load(database_info_file)
+        SQL_QUERIES = json.load(sql_queries_file)
+        USER_DATA = json.load(user_data_file)
+except FileNotFoundError:
+    logger.error('At least one of the config JSON files was not found.')
+    raise FileNotFoundError
+
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,3 +2,26 @@ import logging
 
 
 logger = logging.getLogger("Logger")
+logger.setLevel(logging.DEBUG)
+
+
+cli_handler = logging.StreamHandler()
+cli_handler.setLevel(logging.DEBUG)
+cli_formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%H:%M:%S"
+)
+cli_handler.setFormatter(cli_formatter)
+
+
+file_handler = logging.FileHandler("artifacts/playwright_tests.log", mode="w")
+file_handler.setLevel(logging.INFO)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d at %H:%M:%S"
+)
+file_handler.setFormatter(file_formatter)
+
+
+logger.addHandler(cli_handler)
+logger.addHandler(file_handler)
